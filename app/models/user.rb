90a2f0935c9a04362143306c44a44cf7ba753a22
Joseph An <an.joseph891@gmail.com>
2015-08-27 20:56:45 -0400

    refactor User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,21 +16,21 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
 	# Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
 	# Returns a random token.
-	def User.new_token
+	def self.new_token
 		SecureRandom.urlsafe_base64
 	end
 
 	# Remembers a user in the database for use in persistent sessions.
 	def remember
-		self.remember_token = User.new_token
-		update_attribute(:remember_digest, User.digest(remember_token))
+		self.remember_token = self.new_token
+		update_attribute(:remember_digest, self.digest(remember_token))
 	end
 
 	# Returns true if the given token matches the digest.
